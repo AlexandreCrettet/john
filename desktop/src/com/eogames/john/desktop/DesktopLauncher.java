e63ef7f7cca453c98affa56e6575ccad07dd7553
Alexandre Crettet <alexandre.crettet@epitech.eu>
2016-02-08 21:25:15 +0100

    Commit the initial project

---
+++ desktop/src/com/eogames/john/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eogames.john.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eogames.john.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
