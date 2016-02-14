2e63a1fd76607cf425e0039a10e6e42473891c41
Alexandre Crettet <alexandre.crettet@epitech.eu>
2016-02-14 14:14:48 +0200

    Set the first logic of the levels / map management

--- core/src/com/eogames/john/Main.java
+++ core/src/com/eogames/john/Main.java
@@ -2,26 +2,24 @@ package com.eogames.john;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.assets.AssetManager;
+import com.eogames.john.level.JohnLevelManager;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+  private AssetManager assetManager;
+  private JohnLevelManager levelManager;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+  @Override
+  public void create() {
+    Gdx.app.log("width", Integer.toString(Gdx.graphics.getWidth()));
+    Gdx.app.log("height", Integer.toString(Gdx.graphics.getHeight()));
+
+    assetManager = new AssetManager();
+    levelManager = new JohnLevelManager(assetManager);
+  }
+
+  @Override
+  public void render() {
+    levelManager.render();
+  }
 }
