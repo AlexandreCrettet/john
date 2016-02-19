c99d2f3848c90d8925021e13088b418469274b6a
Alexandre Crettet <alexandre.crettet@epitech.eu>
2016-02-19 14:16:02 +0200

    Add SpriteComponent

---
+++ core/src/com/eogames/john/ecs/components/SpriteComponent.java
@@ -0,0 +1,8 @@
+package com.eogames.john.ecs.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Texture;
+
+public class SpriteComponent implements Component {
+  public Texture sprite = null;
+}
