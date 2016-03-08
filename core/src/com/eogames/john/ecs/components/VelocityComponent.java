f06e255847011e602bc70e8b264bd73d98c5f079
Alexandre Crettet <alexandrecrettet@gmail.com>
2016-03-08 23:47:33 +0100

    Fix the top-walls collision

--- core/src/com/eogames/john/ecs/components/VelocityComponent.java
+++ core/src/com/eogames/john/ecs/components/VelocityComponent.java
@@ -4,8 +4,8 @@ import com.badlogic.ashley.core.Component;
 
 public class VelocityComponent implements Component {
   public float maxX = 150.0f;
-  public float maxY = 1800.0f;
+  public float maxY = 1500.0f;
   public float x = 0.0f;
   public float y = 0.0f;
-  public float gravity = 900.0f;
+  public float gravity = 750.0f;
 }
